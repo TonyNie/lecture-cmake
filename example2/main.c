ecd01f709e3e340567c50e9336fb8298ce7d0b89
Tony.Nie <tony_nie@realsil.com.cn>
2012-05-15 14:20:58 +0800

    add example demo how to use cross-compile environment
    Signed-off-by: Tony.Nie <tony_nie@realsil.com.cn>

---
+++ example2/main.c
@@ -0,0 +1,9 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv)
+{
+	fprintf(stdout, "CMake corss-compile!\n");
+	
+	return 0;
+}
---
+++ example2/toolchain_mipsel.cmake
@@ -0,0 +1,14 @@
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_VERSION 1)
+
+SET(CMAKE_C_COMPILER  /usr/local/toolchain_mipsel/bin/mipsel-linux-gcc) 
+SET(CMAKE_CXX_COMPILER /usr/local/toolchain_mipsel/bin/mipsel-linux-g++)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH  /usr/local/toolchain_mipsel)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
